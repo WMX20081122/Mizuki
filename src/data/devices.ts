c25d2bf28734f04b263175b7d95bb13c3a199f6d
WMX20081122 <2186235192@qq.com>
2026-04-01 00:48:08 +0800

    添加设备数据

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -12,4 +12,14 @@ export type DeviceCategory = Record<string, Device[]> & {
 	自定义?: Device[];
 };
 
-export const devicesData: DeviceCategory = {};
+export const devicesData: DeviceCategory = {
+	"手机": [
+		{
+			name: "OPPO Reno5 K 5G",
+			image: "/images/device/oppo-reno5k.webp",
+			specs: "骁龙750G | 8GB RAM | 60GB 存储",
+			description: "日常主力机，用于通讯、社交、轻度游戏",
+			link: "https://www.oppo.com/cn/"
+		}
+	]
+};
