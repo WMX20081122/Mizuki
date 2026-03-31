e8f39f509620bd87736829d6952a5749ddd59d3e
WMX20081122 <2186235192@qq.com>
2026-04-01 00:46:25 +0800

    添加时间线数据

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,3 +1,19 @@
 import type { TimelineItem } from "../components/features/timeline/types";
 
-export const timelineData: TimelineItem[] = [];
+export const timelineData: TimelineItem[] = [
+	{
+		id: "wmx-server-launch",
+		date: "2024-11-06",
+		title: "WMX 服务器开服",
+		description: "Minecraft 基岩版服务器正式开服，开启新的冒险之旅",
+		category: "milestone",
+		icon: "mdi:minecraft",
+		color: "#62B47A",
+		links: [
+			{
+				label: "了解更多",
+				url: "/about/"
+			}
+		]
+	}
+];
