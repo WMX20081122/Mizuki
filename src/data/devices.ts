f7eff0d9c27cc0d7906feec08026974799ca20aa
WMX20081122 <2186235192@qq.com>
2026-04-01 22:45:43 +0800

    Update devices.ts image paths

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -16,7 +16,7 @@ export const devicesData: DeviceCategory = {
 	"手机": [
 		{
 			name: "OPPO Reno5 K 5G",
-			image: "/images/device/oppo-reno5k.webp",
+			image: "/images/device/oppo-reno5k.jpg",
 			specs: "骁龙750G | 8GB RAM | 128GB 存储",
 			description: "日常主力机，用于通讯、社交、轻度游戏",
 			link: "https://www.oppo.com/cn/"
@@ -25,7 +25,7 @@ export const devicesData: DeviceCategory = {
 	"服务器": [
 		{
 			name: "WMX 服务器",
-			image: "/images/device/server.webp",
+			image: "/images/device/server.jpg",
 			specs: "AMD Ryzen 9 9950X | 8GB RAM | 60GB 存储",
 			description: "Minecraft 基岩版服务器，网站托管",
 			link: "https://wumingx.top/"
